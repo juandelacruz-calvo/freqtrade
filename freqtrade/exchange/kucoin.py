3afdf462c90585b72a36350a796626f2266fac81
Juan Calvo <juandelacruz.calvo@gmail.com>
2022-07-27 23:20:58 +0100

    Make kucoin available on futures

--- freqtrade/exchange/kucoin.py
+++ freqtrade/exchange/kucoin.py
@@ -1,7 +1,8 @@
 """Kucoin exchange subclass."""
 import logging
-from typing import Dict
+from typing import Dict, Tuple, List
 
+from freqtrade.enums import MarginMode, TradingMode
 from freqtrade.exchange import Exchange
 
 
@@ -27,6 +28,14 @@ class Kucoin(Exchange):
         "time_in_force_parameter": "timeInForce",
         "ohlcv_candle_limit": 1500,
     }
+
+    _supported_trading_mode_margin_pairs: List[Tuple[TradingMode, MarginMode]] = [
+        # TradingMode.SPOT always supported and not required in this list
+        # (TradingMode.MARGIN, MarginMode.CROSS),
+        (TradingMode.FUTURES, MarginMode.CROSS),
+        (TradingMode.FUTURES, MarginMode.ISOLATED)
+    ]
+
 
     def stoploss_adjust(self, stop_loss: float, order: Dict, side: str) -> bool:
         """
